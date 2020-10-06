43b4978856129482447945dc7a74e9ffea602fbc
youngminJANG <dudalssnla91@naver.com>
2020-10-06 13:45:25 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>let's develop management system!</h2>
+
+    </div>
+  );
+}
+
+export default App;
